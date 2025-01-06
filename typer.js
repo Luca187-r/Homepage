a3dc8d555f913012ac20a0d2167e6edaec378d98
Luca187-r <Lucaboth2024@gmail.com>
2025-01-06 02:32:27 +0100

    Delete typer.js

--- typer.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed('#title-typer', {
-    strings: ['<span ti=0>Coding</span>', '<span ti=1>Server</span>', '<span ti=3>Minecraft</span>', '<span ti=4>Citybuild</span>', '<span ti=4>Everyone</span>'],
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000,
-    startDelay: 0,
-    showCursor: true,
-    loop: true
-});
